fdee537e0bf5e373e6de2c5ae653381600e2d978
NicoleKuong <nicolekuong@gmail.com>
2020-01-10 11:51:32 +0100

    delete the unnesscary detail-page.html

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,7 +1,9 @@
+//capitalize the first character of the input name
 function captializeFirstLetter(inputName) {
   return inputName.charAt(0).toUpperCase() + inputName.slice(1);
 }
 
+//check whether the name and message pass all validations
 function doesNotPassAllValidation(name, msg) {
   if (!name && !msg) {
     alert("You forgot to fill in your name and message!");
@@ -28,33 +30,38 @@ function doesNotPassAllValidation(name, msg) {
   return false;
 }
 
+// once 'post comment' button clicked, this function runs
 function submitComment() {
+  //input varibles
   const inputField = document.getElementById("name");
   let name = inputField.value;
   const textArea = document.getElementById("msg");
   const msg = textArea.value;
 
+  //call the function to check all the validation
   if (doesNotPassAllValidation(name, msg)) {
     return null; //why return null is needed?
   }
 
+  //call the function to captialize first letter
   name = captializeFirstLetter(name);
 
+  //create elements on browser
   const comment = document.createElement("section");
   const h3 = document.createElement("h3");
   const p = document.createElement("p");
 
+  //attach the input to the created elements
   h3.innerHTML = `${name} said:`;
   p.innerHTML = msg;
   comment.classList.add("comment");
   comment.appendChild(h3);
   comment.appendChild(p);
 
-  console.log(comment);
-
   const commentSection = document.getElementById("comments");
   commentSection.appendChild(comment);
 
+  //reset the name and comment field to empty
   inputField.value = null;
   textArea.value = null;
 }
